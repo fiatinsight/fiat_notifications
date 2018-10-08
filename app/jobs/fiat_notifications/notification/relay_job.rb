c5228efa2bcabe1881bc3598f08a9ad3f963a439
Andrew Haines <andrew@fiatinsight.com>
2018-10-08 14:48:02 -0400

    Built first version

--- app/jobs/fiat_notifications/notification/relay_job.rb
+++ app/jobs/fiat_notifications/notification/relay_job.rb
@@ -2,8 +2,8 @@ class FiatNotifications::Notification::RelayJob < ApplicationJob
   queue_as :default
 
   def perform(notification)
-    # updated_count = ApplicationController.render partial: "system/notifications/users/#{notification.action}", locals: {notification: notification}, formats: [:html]
-    # ActionCable.server.broadcast "notifications:#{notification.recipient_id}", updated_count: updated_count
+    updated_count = ApplicationController.render partial: "system/notifications/users/#{notification.action}", locals: {notification: notification}, formats: [:html]
+    ActionCable.server.broadcast "notifications:#{notification.recipient_id}", updated_count: updated_count
 
     list_item = ApplicationController.render partial: "system/notifications/users/list-item", locals: {notification: notification}, formats: [:html]
     ActionCable.server.broadcast "notifications:#{notification.recipient_id}", list_item: list_item
