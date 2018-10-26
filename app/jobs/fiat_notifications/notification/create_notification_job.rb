c3215430393019bf1d53540d83d51db663983bf3
Andrew Haines <andrew@fiatinsight.com>
2018-10-26 14:30:56 -0400

    Added more Twilio config and Postmark initial work

--- app/jobs/fiat_notifications/notification/create_notification_job.rb
+++ app/jobs/fiat_notifications/notification/create_notification_job.rb
@@ -2,59 +2,34 @@ class FiatNotifications::Notification::CreateNotificationJob < ApplicationJob
   include ActionView::Helpers::TextHelper
   queue_as :default
 
-  def perform(method, notifiable, creator, recipient, action)
-    FiatNotifications::Notification.create(recipient: recipient, creator: creator, action: "mentioned", notifiable: notifiable, action: action)
+  def perform(method, notifiable, creator, recipient, action, notified_user_ids)
 
-    @client = Twilio::REST::Client.new Rails.application.secrets.TWILIO_ACCOUNT_SID, Rails.application.secrets.TWILIO_AUTH_TOKEN
+    # Create a notification in the db, at least...
+    notification = FiatNotifications::Notification.create(recipient: recipient, creator: creator, action: "mentioned", notifiable: notifiable, action: action)
 
-    @client.api.account.messages.create(
-      from: '+17032609664',
-      to: '+17032200874',
-      body: 'Hey there!'
-    )
-    # method.each do |i|
-    #   puts "Method #{i}"
-    # end
+    # Send SMS messages to anyone who should get them
+    notified_user_ids.each do |i|
+      twilio_client = Twilio::REST::Client.new Rails.application.secrets.TWILIO_ACCOUNT_SID, Rails.application.secrets.TWILIO_AUTH_TOKEN
 
-    # Find @mentioned users and put their emails into an array
-    # mentions ||= begin
-    #                regex = /@([\w]+)/
-    #                comment.body.scan(regex).flatten
-    #              end
-    # mentioned_users ||= User.where(username: mentions)
-    #
-    # if mentioned_users.any?
-    #   mentioned_users.each do |user|
-    #     Notification.create(recipient: user, user: comment.user, action: "mentioned", notifiable: comment.message)
-    #
-    #     if user.email_mention?
-    #       if user.internal
-    #         url = "https://my.fiatinsight.com/system/messages/#{comment.message.id}"
-    #       elsif !user.internal
-    #         url = "https://my.fiatinsight.com/client/messages/#{comment.message.id}"
-    #       end
-    #
-    #       client = Postmark::ApiClient.new('5e054db8-4656-4b3a-923c-b3fa28c11e15')
-    #       client.deliver_with_template(
-    #       {:from=>"hello@fiatinsight.com",
-    #        :to=>User.find(user.id).email,
-    #        :reply_to=>"5dfaecfc07476ccff3b32c80c3ba592d+#{comment.message.id}@inbound.postmarkapp.com",
-    #        :template_id=>1263261,
-    #        :template_model=>
-    #         {"commenter_name"=>User.find(comment.user.id).username,
-    #          "subject"=>comment.message.subject,
-    #          "body"=>simple_format(comment.body),
-    #          "url"=>url,
-    #          "timestamp"=>comment.created_at}}
-    #       )
-    #     end
-    #
-    #     if user.slack_mention?
-    #       client = Slack::Web::Client.new
-    #       link = "https://my.fiatinsight.com/system/messages/#{comment.message.id}"
-    #       client.chat_postMessage(channel: "@#{user.slack_username}", text: "New comment by #{User.find(comment.user_id).username} on *#{comment.message.subject}* #{link}: #{comment.body}", as_user: false)
-    #     end
-    #   end
-    # end
+      twilio_client.api.account.messages.create(
+        from: '+17032609664', # This is the phone number for Parish.es
+        to: '+17032200874', # For testing
+        # to: "+1#{User.find(i).phone_number}", # For usage
+        body: "New notification for #{User.find(i).email}: #{recipient.full_name} was #{notification.action}"
+      )
+    end
+
+    # Send emails to anyone who should get them
+    notified_user_ids.each do |i|
+      postmark_client = Postmark::ApiClient.new(Rails.application.secrets.POSTMARK_API_TOKEN)
+      postmark_client.deliver_with_template(
+      {:from=>"notifications@parish.es",
+       :to=>User.find(i).email,
+       :template_id=>8418039,
+       :template_model=>
+        {"subject"=>"Test notification for #{User.find(i).email}",
+         "timestamp"=>notification.created_at}}
+      )
+    end
   end
 end
