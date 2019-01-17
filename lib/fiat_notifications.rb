0422029c9c9300781efff23c23fe0aa26547268b
Andrew Haines <andrew@fiatinsight.com>
2019-01-17 07:31:07 -0500

    Created new accessible attribute for reply to email address

--- lib/fiat_notifications.rb
+++ lib/fiat_notifications.rb
@@ -4,5 +4,6 @@ require 'twilio-ruby'
 module FiatNotifications
   mattr_accessor :from_email_address
   mattr_accessor :email_template_id
+  mattr_accessor :reply_to_email_address
   mattr_accessor :from_phone_number
 end
