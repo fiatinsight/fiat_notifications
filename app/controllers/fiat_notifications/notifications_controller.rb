463a2ac0394a825540d0a9be6cecf0631b7dcb8a
Andrew Haines <andrew@fiatinsight.com>
2019-09-12 14:12:34 -0400

    Cleanup notifications controller

--- app/controllers/fiat_notifications/notifications_controller.rb
+++ app/controllers/fiat_notifications/notifications_controller.rb
@@ -15,23 +15,12 @@ module FiatNotifications
     end
 
     # def mark_all_read
-    #   @user = User.find(params[:user_id])
-    #   @user.notifications.shown.each do |m|
-    #     m.update_attributes(hidden: 1)
-    #   end
-    #
-    #   respond_to do |format|
-    #     format.html { redirect_back(fallback_location: notifications_system_user_path(@user), notice: "Notifications marked as read." ) }
-    #   end
     # end
 
     private
 
       def notification_params
-        params.require(:notification).permit(
-          # :recipient_type, :recipient_id, :creator_type, :creator_id, :action, :notifiable_type, :notifiable_id,
-          :hidden
-        )
+        params.require(:notification).permit(:hidden)
       end
 
   end
